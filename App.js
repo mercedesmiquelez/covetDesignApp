5a4f8bacc33aa0186234066f41e975e3b2e74f88
Mercedes Miquelez <mer_m_@hotmail.com>
2024-04-15 11:01:00 -0300

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, TextInput, Button, StyleSheet, Text } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = () => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.inputContainer}>
+                <TextInput style={styles.input} />
+                <Button title="ADD" color="#463f3a" />
+            </View>
+
+            <View style={styles.taskContainer}>
+                <View style={styles.card}>
+                    <Text style={styles.taskText}>Living</Text>
+                </View>
+                <View style={styles.card}>
+                    <Text style={styles.taskText}>Cocina</Text>
+                </View>
+                <View style={styles.card}>
+                    <Text style={styles.taskText}>Comedor</Text>
+                </View>
+                <View style={styles.card} >
+                    <Text style={styles.taskText}>Dormitorio</Text>
+                </View>
+                <View style={styles.card}>
+                    <Text style={styles.taskText}>Espacios especiales</Text>
+                </View>
+                <View style={styles.card}>
+                    <Text style={styles.taskText}>Galeria exterior</Text>
+                </View>
+                <View style={styles.card}>
+                    <Text style={styles.taskText}>Jardin</Text>
+                </View>
+                <View style={styles.card} >
+                    <Text style={styles.taskText}>Terraza</Text>
+                </View>
+                <View style={styles.card} >
+                    <Text style={styles.taskText}>Verdes</Text>
+                </View>
+            </View>
+        </View>
+    )
 }
 
+export default App;
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        paddingTop: 30,
+        alignItems: "center",
+        backgroundColor: "#f4f3ee",
+        flex: 1
+    },
+    inputContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingHorizontal: 20,
+    },
+    input: {
+        borderBottomWidth: 1,
+        borderBottomColor: "black",
+        width: 250,
+        fontSize: 16
+    },
+    taskContainer: {
+        borderWidth: 1,
+        marginTop: 15,
+        alignItems: "center",
+        width: "90%",
+        paddingVertical: 10
+    },
+    card: {
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#bcb8b1",
+        width: "90%",
+        paddingVertical: 15,
+        marginVertical: 10,
+        borderRadius: 5
+    },
+    taskText: {
+        fontWeight: "bold",
+        fontSize: 17
+    },
+})
